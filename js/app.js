022668c5fad5b9a4aabb7d4a8b98f49ae175cc1d
Dragosh Cr <dragosh.craciun@gmail.com>
2019-08-13 11:43:39 +0200

    Refactor class names

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 fetch('https://api.myjson.com/bins/so5pk').then(res => {
   res.text().then(text =>{
-    document.querySelector(".player").innerHTML = '<source src="http://mirrors.standaloneinstaller.com/video-sample/dolbycanyon.mp4" type="video/mp4">'
+    document.querySelector(".player__video").innerHTML = '<source src="http://mirrors.standaloneinstaller.com/video-sample/dolbycanyon.mp4" type="video/mp4">'
   })
 })
